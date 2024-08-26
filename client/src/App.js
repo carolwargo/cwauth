5d9e27904715d82fe16c484ada296966a909659a
Carol Wargo <Carolwargo@gmail.com>
2024-08-26 11:08:39 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,42 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {UserContextProvider} from "./UserContext";
+import { UserContextProvider } from "./UserContext";
+import BlackHeader from "./components/Header/BlackHeader";
 
 import AdminPage from "./pages/AdminPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import ErrorBoundary from './ErrorBoundary';
+import ErrorBoundary from "./ErrorBoundary";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
-import HomeLayout from "./components/Layout/HomeLayout.jsx";
 import AdminLayout from "./components/Layout/AdminLayout.jsx";
-import MultiDropNav from "./components/Navgation/MultiDropNav.jsx";  
+import MultiDropNav from "./components/Navgation/MultiDropNav.jsx";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-    <ErrorBoundary>
-    <MultiDropNav />
-    <UserContextProvider>
-    <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home-layout" element={<HomeLayout />}>
-                <Route index element={<HomePage />} />
-              </Route>
-
+        <ErrorBoundary>
+          <MultiDropNav />
+          <BlackHeader />
+          <UserContextProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
               <Route path="/admin-layout" element={<AdminLayout />}>
                 <Route index element={<AdminPage />} />
               </Route>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-            {/* Fallback route to catch all unmatched paths */}
-            <Route path="*" element={<NotFound />} />
-      </Routes>
-    </UserContextProvider>
-    </ErrorBoundary>
-  </BrowserRouter>
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+           
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </UserContextProvider>
+        </ErrorBoundary>
+      </BrowserRouter>
     </div>
   );
 }
@@ -51,4 +48,3 @@ function NotFound() {
 }
 
 export default App;
-
